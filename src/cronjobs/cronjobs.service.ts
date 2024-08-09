59ecaaf91b4c852b91b3419051928bb55d667a9a
janani811 <jananigopalsanthi@gmail.com>
2024-08-09 22:50:46 +0530

    cronjob for notification

--- src/cronjobs/cronjobs.service.ts
+++ src/cronjobs/cronjobs.service.ts
@@ -13,15 +13,15 @@ export class CronjobsService {
   async openForBusiness() {
     console.log('Delicious cakes is open for business...');
   }
-  // @Cron('0 55 13 * * *')
-  // async generateNotifications() {
-  //   const users = await this.userRepository.getUserWithLendsCount();
-  //   for (let i = 0; i < users.length; i++) {
-  //     await this.notificationRepository.add({
-  //       nt_text: 'The total pending payments is ',
-  //       nt_pending_count: users[i].pending_installment_count,
-  //       nt_user_id: users[i].us_id,
-  //     });
-  //   }
-  // }
+  @Cron('0 55 13 * * *')
+  async generateNotifications() {
+    const users = await this.userRepository.getUserWithLendsCount();
+    for (let i = 0; i < users.length; i++) {
+      await this.notificationRepository.add({
+        nt_text: 'The total pending payments is ',
+        nt_pending_count: users[i].pending_installment_count,
+        nt_user_id: users[i].us_id,
+      });
+    }
+  }
 }
