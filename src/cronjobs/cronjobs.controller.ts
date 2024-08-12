f938132e95cd87efdd7d690d42ee2aff23dd7bb9
sachinelavarasan <sachinelavarasan321@gmail.com>
2024-08-12 09:31:45 +0530

    testing cron job

--- src/cronjobs/cronjobs.controller.ts
+++ src/cronjobs/cronjobs.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request, Response } from '@nestjs/common';
 
 import { CronjobsService } from './cronjobs.service';
 
@@ -7,7 +7,9 @@ export class CronjobsController {
   constructor(private readonly cronjobsService: CronjobsService) {}
 
   @Get()
-  async getHello() {
+  async runCron(@Request() req, @Response() res) {
+    console.log('runCron');
+    res.status(200).json('Cron initiaited');
     await this.cronjobsService.openForBusiness();
     await this.cronjobsService.generateNotifications();
     await this.cronjobsService.sendNotification();
