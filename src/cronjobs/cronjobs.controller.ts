fd52ba35404c0a6b7d8d84f2226f46c2d17c2edd
sachinelavarasan <sachinelavarasan321@gmail.com>
2025-08-12 10:04:33 +0530

    Fix: add reomve cron

--- src/cronjobs/cronjobs.controller.ts
+++ src/cronjobs/cronjobs.controller.ts
@@ -13,7 +13,14 @@ export class CronjobsController {
       // res.status(200).json('Cron initiaited');
       // await this.cronjobsService.generateNotifications();
       // await this.cronjobsService.sendNotification();
-      await this.cronjobsService.expensifySendNotification();
+      // await this.cronjobsService.expensifySendNotification();
+      //TODO: To add cron job in vercel.json need add this
+      // "crons": [
+      //   {
+      //     "path": "/crons",
+      //     "schedule": "0 11 * * *"
+      //   }
+      // ]
       console.log('********* Pending Notification Reminder Completed ********');
     } catch (error) {
       console.error(error);
